f77e62cadc99b4916bb72f3c6f253b61ec5a54e8
Hui Tang <htang085@uottawa.ca>
2025-01-10 02:02:45 -0800

    Initial commit

---
+++ R/add_numbers.R
@@ -0,0 +1,11 @@
+#' Add Two Numbers
+#'
+#' This function adds two numbers and returns the result.
+#'
+#' @param a A numeric value.
+#' @param b A numeric value.
+#' @return The sum of a and b.
+#' @export
+add_numbers <- function(a, b) {
+  return(a + b)
+}
